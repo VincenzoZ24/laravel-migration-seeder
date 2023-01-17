d27519024d096f654f49c42778bf9c46130107d2
VincenzoZ24 <zitov287@gmail.com>
2023-01-17 17:28:18 +0100

    first commit

---
+++ database/migrations/2023_01_17_154459_create_trains_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda', 100);
+            $table->string('stazione di partenza', 100);
+            $table->string('stazione di arrivo', 100);
+            $table->time('orario di partenza');
+
+            $table->timestamps();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
